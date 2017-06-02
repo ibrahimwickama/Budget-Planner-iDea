d18bd8413d83a4055056e1f39de877c982cd6721
Ibrahim Wickama <ibrahimwickama@gmail.com>
2017-06-02 15:39:48 +0300

    phase2

--- app/src/main/java/com/wickerlabs/exp_tr/Profile.java
+++ app/src/main/java/com/wickerlabs/exp_tr/Profile.java
@@ -12,7 +12,7 @@ public class Profile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.prof);
+        setContentView(R.layout.activity_profile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
