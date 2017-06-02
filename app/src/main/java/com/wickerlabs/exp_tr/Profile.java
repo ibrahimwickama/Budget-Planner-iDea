4c44b7ce6929b5ab647deb22d03d077d180c38ba
Ibrahim Wickama <ibrahimwickama@gmail.com>
2017-06-02 16:47:48 +0300

    phase2

--- app/src/main/java/com/wickerlabs/exp_tr/Profile.java
+++ app/src/main/java/com/wickerlabs/exp_tr/Profile.java
@@ -6,12 +6,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
 
 public class Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        //getActionBar().hide();
         setContentView(R.layout.activity_profile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -20,7 +23,7 @@ public class Profile extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Edit Profile Action ", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
